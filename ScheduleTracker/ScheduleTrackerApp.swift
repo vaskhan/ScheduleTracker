61c3a22b27abd5f5285b8ea9c170c06964cca4f8
Vasiliy Khanin <ya-v.khanin@yandex.ru>
2025-06-12 15:16:48 +0300

    Initial commit

---
+++ ScheduleTracker/ScheduleTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleTrackerApp.swift
+//  ScheduleTracker
+//
+//  Created by Василий Ханин on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
