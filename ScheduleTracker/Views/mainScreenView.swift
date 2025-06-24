ac550cf4c6404cc6da77686b40afcc7555ef61e0
Vasiliy Khanin <ya-v.khanin@yandex.ru>
2025-06-25 00:36:45 +0300

    maked mainScreen

--- ScheduleTracker/Views/mainScreenView.swift
+++ ScheduleTracker/Views/mainScreenView.swift
@@ -9,31 +9,82 @@ import SwiftUI
 
 struct mainScreenView: View {
     @State private var viewModel = MockReelsModel()
+    @State private var fromTofromTo = true
     
     var body: some View {
         TabView {
-            ZStack {
-                VStack {
-                    ScrollView (.horizontal, showsIndicators: false) {
-                        LazyHStack(alignment: .center, spacing: 12) {
-                            ForEach(viewModel.reels) { reels in
-                                ReelsCell(reels: reels)
+            VStack (spacing: 44){
+                ScrollView (.horizontal, showsIndicators: false) {
+                    LazyHStack(alignment: .center, spacing: 12) {
+                        ForEach(viewModel.reels) { reels in
+                            ReelsCell(reels: reels)
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                    
+                }
+                
+                .frame(height: 140)
+                
+                ZStack {
+                    Color("blueUniversal")
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    HStack {
+                        VStack(alignment: .leading, spacing: 0) {
+                            Button(action: {}) {
+                                Text(fromTofromTo ? "Откуда" : "Куда")
+                                    .foregroundColor(Color("grayUniversal"))
+                                    .padding(.vertical, 14)
+                                    .padding(.horizontal, 16)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                
+                            }
+                            
+                            Button(action: {}) {
+                                Text(fromTofromTo ? "Куда" : "Откуда")
+                                    .foregroundColor(Color("grayUniversal"))
+                                    .padding(.vertical, 14)
+                                    .padding(.horizontal, 16)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
                             }
                         }
+                        .background(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.white)
+                        )
                         .padding(.horizontal, 16)
+                        
+                        Button(action: { fromTofromTo.toggle() }) {
+                            Image(systemName: "arrow.2.squarepath")
+                                .font(.system(size: 24))
+                                .foregroundColor(Color("blueUniversal"))
+                                .padding(6)
+                                .background(.white)
+                                .clipShape(Circle())
+                        }
+                        .padding(.trailing, 16)
                     }
-                    Spacer()
+                    .padding(.vertical, 16)
                 }
-                .frame(maxHeight: .infinity, alignment: .top)
+                .frame(height: 128)
+                .padding(.horizontal, 16)
+                
+                Spacer()
+                
+                Divider()
+                    .frame(height: 3)
             }
+            .padding(.top, 24)
             .tabItem {
-                Label("", systemImage: "1.square.fill")
+                Image("scheduleTabItem")
             }
+            
             ZStack {
                 //page2
             }
             .tabItem {
-                Label("", systemImage: "2.square.fill")
+                Image("gearTabItem")
             }
         }
     }
